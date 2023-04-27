5c798cce68986b511c82665e0ff0797829a40cf9
josed5001 <joderas@s.sfusd.edu>
2023-04-27 13:20:10 -0700

    Prefab tagged

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject easyDifficulty;
+    public GameObject mediumDifficulty;
+
+    private float timeElapsed = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        easyDiff();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeElapsed = Time.time;
+
+        if (timeElapsed > 10f)
+        {
+            
+        }
+    }
+
+    void easyDiff()
+    {
+        Vector3 aSpawnPos = new Vector3(0, 0, 0);
+
+        Instantiate(easyDifficulty, aSpawnPos, easyDifficulty.gameObject.transform.rotation);
     }
 }
