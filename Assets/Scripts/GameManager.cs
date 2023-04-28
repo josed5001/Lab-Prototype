580da43b9e0d80045bd431202f6b3ff302ca8bb8
josed5001 <joderas@s.sfusd.edu>
2023-04-28 13:35:32 -0700

    Medium Difficulty

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     public GameObject mediumDifficulty;
 
     private float timeElapsed = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +22,10 @@ public class GameManager : MonoBehaviour
 
         if (timeElapsed > 10f)
         {
-            
+            destroyLevelWithTag("Level");
+    
+            medDiff();
+           
         }
     }
 
@@ -30,5 +34,21 @@ public class GameManager : MonoBehaviour
         Vector3 aSpawnPos = new Vector3(0, 0, 0);
 
         Instantiate(easyDifficulty, aSpawnPos, easyDifficulty.gameObject.transform.rotation);
+    }
+
+    void medDiff()
+    {
+        Vector3 aSpawnPos = new Vector3(0, 0, 0);
+
+        Instantiate(mediumDifficulty, aSpawnPos, mediumDifficulty.gameObject.transform.rotation);
+    }
+
+    void destroyLevelWithTag(string tag)
+    {
+        GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag(tag);
+        foreach (GameObject taggedObject in taggedObjects)
+        {
+            Destroy(taggedObject);
+        }
     }
 }
