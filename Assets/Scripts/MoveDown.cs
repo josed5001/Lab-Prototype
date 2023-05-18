0cf8802df2be708c54aba84ae13d878c866d22b4
josed5001 <joderas@s.sfusd.edu>
2023-05-18 12:31:27 -0700

    Diff Speed

--- Assets/Scripts/MoveDown.cs
+++ Assets/Scripts/MoveDown.cs
@@ -5,30 +5,29 @@ using UnityEngine;
 public class MoveDown : MonoBehaviour
 {
 
-    public float speed = 2.5f;
+    
     private float zDestroy = -110.0f;
-
+    
+    private float timeDelay = 14.5f;
+    private GameDifficulty gameDifficulty;
     private SpawnManager spawnScript;
     private Rigidbody objectRb;
 
     // Start is called before the first frame update
     void Start()
     {
+        gameDifficulty = GameObject.Find("GameDifficulty").GetComponent<GameDifficulty>();
         spawnScript = GameObject.Find("Spawn Manager").GetComponent<SpawnManager>();
         objectRb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (spawnScript.ObSpawnChange == true)
-        {
-            speed = 10f;
-        }
+        
 
-           
-
-        objectRb.AddForce(Vector3.forward * -speed);
+        objectRb.AddForce(Vector3.forward * -gameDifficulty.speed);
 
 
         if (transform.position.z < zDestroy)
@@ -36,4 +35,6 @@ public class MoveDown : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    
 }
